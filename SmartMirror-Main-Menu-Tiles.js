686143fc375f1e733c377be9badb570a379ab9c3
Nils Kucza <nkucza@cit-ec.uni-bielefeld.de>
2023-04-13 11:19:04 +0200

    Change tile background color

--- SmartMirror-Main-Menu-Tiles.js
+++ SmartMirror-Main-Menu-Tiles.js
@@ -16,7 +16,7 @@ Module.register("SmartMirror-Main-Menu-Tiles", {
 				utilities: { title: "Utilities", icon: "fa fa-clone" },
 				campus: { title: "Campus Area", icon: "fa fa-university" },
 				entertainment: { title: "Entertainment", icon: "fa fa-star" },
-				smarthome: { title: "Smart Home", icon: "fa fa-home" },
+				//smarthome: { title: "Smart Home", icon: "fa fa-home" },
 				preferences: { title: "Preferences", icon: "fa fa-cogs" },
 				// Example of a menu entry which shows a radial menu for integer user input and sends result as string to the given topic
 				// integerinput: { title: "Integer Input", icon: "fa fa-dot-circle", input: { type: "integer", topic: "SHOW_ALERT", value: 10, min: 0, max: 100, steps: 8 } },
@@ -38,8 +38,8 @@ Module.register("SmartMirror-Main-Menu-Tiles", {
 				clock: { title: "Clock", icon: "fa fa-clock-o" },
 				calendar: { title: "Calendar", icon: "fa fa-calendar" },
 				weather: { title: "Current Weather", icon: "fa fa-cloud" },
-				bivital: { title: "Vital Data", icon: "fa fa-heartbeat" },
-				speech: { title: "Speech Recogn. Output", icon: "fa fa-comment" },
+				//bivital: { title: "Vital Data", icon: "fa fa-heartbeat" },
+				//speech: { title: "Speech Recogn. Output", icon: "fa fa-comment" },
 				back: { title: "Back", icon: "fa fa-undo" },
 			},
 			campus: {
@@ -101,7 +101,8 @@ Module.register("SmartMirror-Main-Menu-Tiles", {
 	// Colors
 	menuColor: 'rgb(0, 0, 0)', // Menu element color
 	menuBorderColor: 'rgb(255, 255, 255)', // Menu element border color
-	hoverColor: 'rgb(63, 63, 63)', // Color of hovered element
+	//hoverColor: 'rgb(63, 63, 63)', // Color of hovered element
+	hoverColor: 'rgb(0, 0, 0)', // Color of hovered element
 	hoverDistanceColor: 'rgb(230, 230, 230)', // Color of distance feedback
 	blinkColor: 'rgb(230, 230, 230)', // Color of selected element blink feedback
 	// Constants
